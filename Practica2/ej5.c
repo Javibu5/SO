87f2f985b7e7bd0553643d3740a774932f0cc97c
Javibu5 <adfjb77@gmail.com>
2017-11-22 17:47:36 +0100

    .

--- Practica2/ej5.c
+++ Practica2/ej5.c
@@ -11,21 +11,30 @@
 void * Calculo(int matrizFuncion[2][3]){
 	
 	int *x = (int *)malloc(sizeof(int));
-	
+	*x = 0;
+
 	int multiplicacion;
-	for (int i = 0; i < ; ++i)
+
+	for (int i = 0; i < 3 ; ++i)
 	{
-		*x =  matrizFuncion[0]
+		multiplicacion =  matrizFuncion[0][i] * matrizFuncion[1][i];
+		*x =+ multiplicacion;
 	}
+
+	printf("%d\n" , *x);
 	
+	pthread_exit(x);
 
 }
 
 int main()
 {
-	int m[3][3]= {0 , 1 , 2} , {3 , 4 , 1} , {5 , 1 , 2};
+	int m[3][3]= {{0 , 1 , 2},{3 , 4 , 1},{5 , 1 , 2}};
 	int v[3] = { 2 , 0 , 1};
 	int maux[2][3];
+
+	int * suma;
+	int resultado = 0;
 
 
 	
@@ -36,9 +45,19 @@ int main()
 		{
 			maux[0][j] = m[i][j];
 			maux[1][j] = v[i];
+			pthread_create (&thd[i], NULL, (void *) Calculo, &maux);
 		}
-		pthread_create (&thd[i], NULL, (void *) Calculo, &maux);
+		
 	}
+
+	for(int j = 0 ; j<3 ; j++){
+		pthread_join(thd[j], (void **) &suma);
+		printf("El hilo %d ha devuelto el valor %d\n",j, *suma);
+		resultado =+ *suma;
+	}
+
+	printf("%d\n", resultado);
+	 pthread_exit(NULL);
 
 
 
